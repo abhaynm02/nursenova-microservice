ec42c07ca5f8adb0bd4105c6673664b149e14f9f
abhaynm02 <abhaybhaskarnm02@gmail.com>
2024-05-31 11:56:08 +0530

    api gateway added

---
+++ nursenova-backend/nursenova-gateway_/src/main/java/com/abhay/nursenova_gateway/NursenovaGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.abhay.nursenova_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NursenovaGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NursenovaGatewayApplication.class, args);
+	}
+
+}
---
+++ nursenova-backend/nursenova-gateway_/src/test/java/com/abhay/nursenova_gateway/NursenovaGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhay.nursenova_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NursenovaGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
